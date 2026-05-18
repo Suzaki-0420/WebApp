54a9ae3a4a95cba8917a0502c9d68597be36facf
Suzaki0420 <chihoko.suzaki@wemex.com>
2026-05-18 15:49:32 +0900

    25

--- WebApp_Exercise/Infrastructures/Entities/ItemEntity.cs
+++ WebApp_Exercise/Infrastructures/Entities/ItemEntity.cs
@@ -7,7 +7,7 @@ public class ItemEntity
 {
     [Key]
     [Column("id")]
-    public int? Id { get; set; }
+    public int Id { get; set; }
 
     [Column("name")]
     public string? Name { get; set; }
